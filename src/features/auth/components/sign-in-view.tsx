05d1d40695c6c366107f18c5dc0c78f54da18e8b
PerHac13 <abdullah13112004@gmail.com>
2025-05-18 13:46:54 +0530

    feat!: auth setup and profile routes using clerk

---
+++ src/features/auth/components/sign-in-view.tsx
@@ -0,0 +1,77 @@
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { SignIn as ClerkSignInForm } from '@clerk/nextjs';
+import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Authentication',
+  description: 'Authentication forms built using the components.'
+};
+
+export default function SignInViewPage() {
+  return (
+    <div className='relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
+      <Link
+        href='/examples/authentication'
+        className={cn(
+          buttonVariants({ variant: 'ghost' }),
+          'absolute top-4 right-4 hidden md:top-8 md:right-8'
+        )}
+      >
+        Login
+      </Link>
+      <div className='bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r'>
+        <div className='absolute inset-0 bg-zinc-900' />
+        <div className='relative z-20 flex items-center text-lg font-medium'>
+          {/* <Image
+            src='/favicon.svg'
+            height={25}
+            width={25}
+            alt={'logo'}
+            className='py-4'
+          /> */}
+          AIonica
+        </div>
+        <div className='relative z-20 mt-auto'>
+          <blockquote className='space-y-2'>
+            <p className='text-lg'>
+              &ldquo;The missing link between your AI investments and actual
+              productivity — clients report 10x faster implementation after
+              switching to our unified interface.&rdquo;
+            </p>
+            <footer className='text-sm'>AIonica</footer>
+          </blockquote>
+        </div>
+      </div>
+      <div className='flex h-full items-center justify-center p-4 lg:p-8'>
+        <div className='flex w-full max-w-md flex-col items-center justify-center space-y-6'>
+          <ClerkSignInForm
+            initialValues={{
+              emailAddress: 'your_mail+clerk_test@example.com'
+            }}
+          />
+
+          <p className='text-muted-foreground px-8 text-center text-sm'>
+            By clicking continue, you agree to our{' '}
+            <Link
+              href='/terms'
+              className='hover:text-primary underline underline-offset-4'
+            >
+              Terms of Service
+            </Link>{' '}
+            and{' '}
+            <Link
+              href='/privacy'
+              className='hover:text-primary underline underline-offset-4'
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
